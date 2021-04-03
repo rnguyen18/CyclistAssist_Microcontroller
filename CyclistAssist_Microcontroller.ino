29924f1a14bada31df1d9b310e2cc348af836b35
Ryan Nguyen <drmrryan18@gmail.com>
2021-04-03 14:10:30 -0500

    Cleaned up the file and implemented libraries

--- CyclistAssist_Microcontroller.ino
+++ CyclistAssist_Microcontroller.ino
@@ -1,24 +1,18 @@
 #include <SoftwareSerial.h>
-#include <LightController.h>
 #include <BluetoothController.h>
+#include <LightController.h>
 
-int rxPin = 52;
-int txPin = 50;
-int frontPin = 53;
-int rearPin = 53;
-int leftPin = 51;
-int rightPin = 53;
-char sig = 0;
-int start = 0;
-SoftwareSerial mySerial(rxPin,txPin); 
+int rxPin = 12;
+int txPin = 10;
+int headlightPin = 5;
+int leftPin = 4;
+int rightPin = 3;
+
+BluetoothController bluetoothController(rxPin, txPin);
+LightController lightController(headlightPin, leftPin, rightPin);
 
 void setup()
 {
-  pinMode(leftPin, OUTPUT);
-  pinMode(rightPin, OUTPUT);
-  pinMode(rxPin, INPUT);
-  pinMode(txPin, OUTPUT);
-  mySerial.begin(9600);   
   Serial.begin(9600);  
   Serial.write("Start");
   delay(100);
@@ -27,28 +21,9 @@ void setup()
 void loop()
 {
   if (Serial.available()>0) {
-    mySerial.write(Serial.read());
+    bluetoothController.Write(Serial.read());
   }
-  if (mySerial.available()>0){
-    sig = mySerial.read();
-    start = millis();
-    Serial.write(sig);
-  }
-  if ((millis() - start) % 1000 < 500) {
-    if (sig == 'R') {
-      digitalWrite(leftPin, HIGH);
-      digitalWrite(rightPin, LOW);
-    }
-    else if (sig == 'L') {
-      digitalWrite(rightPin, HIGH);
-      digitalWrite(leftPin, LOW);
-    }
-    else if (sig == '3') {
-      digitalWrite(leftPin, HIGH);
-      digitalWrite(rightPin, HIGH);
-    }
-  } else {
-      digitalWrite(leftPin, LOW);
-      digitalWrite(rightPin, LOW);
-  }
+
+  lightController.UpdateInput(bluetoothController.Read());
+  lightController.Update();
 }
