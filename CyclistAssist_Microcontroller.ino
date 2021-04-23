905789c98295d7b0f6726ba6bdd63a856f87d6f9
Ryan Nguyen <drmrryan18@gmail.com>
2021-04-23 00:28:14 -0500

    FINISHED

--- CyclistAssist_Microcontroller.ino
+++ CyclistAssist_Microcontroller.ino
@@ -1,29 +1,66 @@
 #include <SoftwareSerial.h>
 #include <BluetoothController.h>
 #include <LightController.h>
+#include <SpeedometerController.h>
+#include <BrakeLightController.h>
+#include <BlindspotDetector.h>
+#include <String.h>
 
+// Bluetooth
 int rxPin = 12;
-int txPin = 10;
-int headlightPin = 5;
-int leftPin = 4;
-int rightPin = 3;
+int txPin = 13;
 
+// Speedometer
+int speedPin = 8;
+
+// Brake Light
+int sensorPin = 7;
+int brakePin = 6;
+
+// Head Lights
+int headlightPin = 5;
+int rearPin = 9;
+
+// Turn Signals
+int leftPin = 3;
+int rightPin = 4;
+
+// Radar
+int triggerPinA = 28;
+int echoPinA = 29;
+int triggerPinB = 37;
+int echoPinB = 36;
+unsigned long radarTime = 0;
+
+BlindspotDetector blindspotDetector(triggerPinA, echoPinA, triggerPinB, echoPinB);
 BluetoothController bluetoothController(rxPin, txPin);
-LightController lightController(headlightPin, leftPin, rightPin);
+LightController lightController(headlightPin, leftPin, rightPin, rearPin);
+SpeedometerController speedometerController(speedPin);
+BrakeLightController brakeLightController(sensorPin, brakePin);
 
 void setup()
-{
-  Serial.begin(9600);  
-  Serial.write("Start");
+{ 
+  Serial.begin(9600);
+  Serial.write("Start\n");
   delay(100);
 }
 
 void loop()
 {
-  if (Serial.available()>0) {
-    bluetoothController.Write(Serial.read());
-  }
+  brakeLightController.Update();
 
   lightController.UpdateInput(bluetoothController.Read());
   lightController.Update();
+  
+   int speedData = speedometerController.Update();
+  if (speedData >= 0) {
+    bluetoothController.WriteString("S" + String(speedData) + " ");
+  }
+  
+  bluetoothController.Update();
+
+  if ((millis() - radarTime + 500) > 1000) {
+    radarTime = millis() + 500;
+    bluetoothController.WriteString("R" + String(blindspotDetector.Update()) + " ");
+  }
 }
